339fb5a41dd44485d5c8400f3a8e972248a1d1b8
kraljic <marko.kraljic10@gmail.com>
2022-08-10 15:38:09 +0200

    Added out buffer to distinguish memory locations

--- Core/Src/keyboard_driver.cpp
+++ Core/Src/keyboard_driver.cpp
@@ -18,28 +18,27 @@ using namespace macro_keyboard;
 
 #define REPORT_ID 1
 #define MACRO_DATA_ADR ((void*)FLASH_PAGE(64))
+#define REPORT_SIZE (sizeof(keyboard_api::KeyboardReport))
 
 extern UART_HandleTypeDef huart1;
 extern UART_HandleTypeDef huart2;
 extern USBD_HandleTypeDef hUsbDeviceFS;
 
 uint8_t ps2ScanCodeBuffer = 0;
-char outBuffer[128] = {0};
+uint8_t outBuffer[4][REPORT_SIZE] = {0};
+uint8_t outBufferIndex = 0;
+
 
 void HAL_UART_RxCpltCallback(UART_HandleTypeDef *husart) {
 	MacroKeyboard::receivePs2Data(ps2ScanCodeBuffer);
 }
 
 void send_report_to_usb(keyboard_api::KeyboardReport *kr) {
-	static keyboard_api::KeyboardReport krStatic = {0};
-	krStatic = *kr;
+    memcpy(outBuffer[outBufferIndex], kr, REPORT_SIZE);
 
-	USBD_HID_SendReport(&hUsbDeviceFS, (uint8_t*) &krStatic, 8);
+	USBD_HID_SendReport(&hUsbDeviceFS, outBuffer[outBufferIndex], REPORT_SIZE);
 
     HAL_Delay(1);
-
-//	hid_keyboard::dumpHidKeyboardReport(&krStatic, outBuffer);
-//	HAL_UART_Transmit_IT(&huart2, (uint8_t *)outBuffer, strlen(outBuffer));
 }
 
 void setup() {
